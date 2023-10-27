b2e7379e458c50b072b7b13675a7af4b6a5551fb
Armand WADJI <armand_wadji@yahoo.fr>
2023-10-27 10:58:19 +0200

    fix du bug de re création des pommes lorsque le jeux est en pause

--- apples.js
+++ apples.js
@@ -5,13 +5,15 @@ class Apples{
     ctx;
     snakeBody;
     applesTable = [];
+    isPaused;
 
-    constructor ( blockSize, canvasWidth, canvasHeight, ctx, snakeBody ) {
+    constructor ( blockSize, canvasWidth, canvasHeight, ctx, snakeBody, isPaused ) {
         this.size = blockSize;
         this.canvasWidth = canvasWidth;
         this.canvasHeight = canvasHeight;
         this.ctx = ctx;
         this.snakeBody = snakeBody;
+        this.isPaused = isPaused;
 
         this.createApples();
     }
@@ -21,8 +23,8 @@ class Apples{
      */
     createApples () {
 
-        this.firstApple = new Apple(this.size, this.canvasWidth, this.canvasHeight, this.ctx, this.snakeBody );
-        this.secondApple = new Apple(this.size, this.canvasWidth, this.canvasHeight, this.ctx, this.snakeBody );
+        this.firstApple = new Apple(this.size, this.canvasWidth, this.canvasHeight, this.ctx, this.snakeBody, this.isPaused );
+        this.secondApple = new Apple(this.size, this.canvasWidth, this.canvasHeight, this.ctx, this.snakeBody, this.isPaused );
         
         if ( this.firstApple.getX() === this.secondApple.getX() || this.firstApple.getY() === this.secondApple.getY() ) { // if the apples are side by side
             this.createApples()
--- snake.js
+++ snake.js
@@ -60,7 +60,7 @@ window.onload = () => {
             [4, 4]
         ], "right" ); 
 
-        apples = new Apples( blockSize, canvasWidth, canvasHeight, ctx, snakee.body );   
+        apples = new Apples( blockSize, canvasWidth, canvasHeight, ctx, snakee.body, isPaused );   
     }
 
     // Create a function that create snake body
@@ -115,8 +115,9 @@ window.onload = () => {
 
     // Create a function that moove the snake
     function mooveSnake() {
-        const head = [...snakee.body[0]]; // Copy the head's position
-        if (!isPaused) {
+        const head = [ ...snakee.body[ 0 ] ];
+        // Copy the head's position
+        if ( !isPaused ) {
             switch (snakee.direction) {
                 case "up":
                     head[1] -= 1;
@@ -191,6 +192,11 @@ window.onload = () => {
         isPaused = !isPaused;
         const buttonText = isPaused ? "Play" : "Pause";
         pauseButton.innerHTML = buttonText;
+
+        apples.applesTable.forEach( apple => {
+            // console.log(apple);
+            apple.setIsPaused(isPaused);
+        } );
     }
     // add pause() and resume() on pauseButton
     pauseButton.addEventListener("click", togglePause);
@@ -200,7 +206,7 @@ window.onload = () => {
         if (isPaused) {
             togglePause();
         }
-        clearInterval(timeInterval);
+        clearInterval( timeInterval );
         timeInterval = setInterval(mooveSnake, delay);
         init();
     }
